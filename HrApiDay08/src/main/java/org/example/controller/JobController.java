ce996563cd5dc57a9f475c195e3e88de4e8c40ab
Raghad Mohammed Aladawi <109043585+adawi-r@users.noreply.github.com>
2024-06-08 23:10:39 +0300

    HrApiDay08

---
+++ HrApiDay08/src/main/java/org/example/controller/JobController.java
@@ -0,0 +1,160 @@
+package org.example.controller;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
+import org.example.DAO.JobDAO;
+import org.example.dto.JobDto;
+import org.example.dto.JobFilterDto;
+import org.example.exceptions.DataNotFoundException;
+import org.example.models.Job;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.net.URI;
+
+
+@Path("/jobs")
+public class JobController {
+
+    JobDAO dao = new JobDAO();
+    @Context UriInfo uriInfo;
+    @Context HttpHeaders headers;
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response selectAllJob(
+              @BeanParam JobFilterDto filter
+            ) {
+        try {
+            GenericEntity<ArrayList<Job>> job = new GenericEntity<ArrayList<Job>>(dao.selectAllJob(filter)) {
+            };
+            if (headers.getAcceptableMediaTypes().contains(MediaType.valueOf(MediaType.APPLICATION_XML))) {
+                return Response
+                        .ok(job)
+                        .type(MediaType.APPLICATION_XML)
+                        .build();
+
+            } else if(headers.getAcceptableMediaTypes().contains(MediaType.valueOf("text/csv"))) {
+                return Response
+                        .ok(job)
+                        .type("text/csv")
+                        .build();
+            }
+            return Response
+//                    .ok(job)
+//                    .type(MediaType.APPLICATION_JSON)
+                    .ok(job, MediaType.APPLICATION_JSON)
+                    .build();
+
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GET
+    @Path("{jobId}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "text/csv"})
+
+    public Response selectJob(@PathParam("jobId") int jobId) throws SQLException{
+
+        try {
+            Job job = dao.selectJob(jobId);
+            if (job == null) {
+                throw new DataNotFoundException("Job " + jobId + "Not found");
+            }
+
+            JobDto dto = new JobDto();
+            dto.setJob_id(job.getJob_id());
+            dto.setJob_title(job.getJob_title());
+            dto.setMin_salary(job.getMin_salary());
+            dto.setMax_salary(job.getMax_salary());
+            addLinks(dto);
+
+            return Response.ok(dto).build();
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addLinks (JobDto dto){
+        URI selfUri = uriInfo.getAbsolutePath();
+        URI empsUri = uriInfo.getAbsolutePathBuilder().path(JobController.class).build();
+
+        dto.addLink(selfUri.toString(), "self");
+        dto.addLink(empsUri.toString(),"employees");
+    }
+
+    @DELETE
+    @Path("{jobId}")
+    public void deleteJob(@PathParam("jobId") int jobId){
+
+        try {
+            dao.deleteJob(jobId);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @POST
+//    @Consumes(MediaType.APPLICATION_XML)
+    public Response insertJob(Job job){
+
+        try {
+            dao.insertJob(job);
+            NewCookie cookie = (new NewCookie.Builder("username")).value("OOOOO").build();
+            URI uri = uriInfo.getAbsolutePathBuilder().path(job.getJob_id() + "").build();
+
+            return Response
+//                    .status(Response.Status.CREATED)
+                    .created(uri)
+//                    .cookie(new NewCookie("username", "OOOOO"))
+                    .cookie(cookie)
+                    .header("Created by", "Raghad")
+                    .build();
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+//
+    @POST
+    public Response insertJobFromForm(@BeanParam Job job){
+
+        try {
+            dao.insertJob(job);
+            NewCookie cookie = (new NewCookie.Builder("username")).value("OOOOO").build();
+            URI uri = uriInfo.getAbsolutePathBuilder().path(job.getJob_id() + "").build();
+
+            return Response
+//                    .status(Response.Status.CREATED)
+                    .created(uri)
+//                    .cookie(new NewCookie("username", "OOOOO"))
+                    .cookie(cookie)
+                    .header("Created by", "Raghad")
+                    .build();
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PUT
+    @Path("{jobId}")
+    public void updateJob(@PathParam("jobId") int jobId, Job job){
+
+        try {
+            job.setJob_id(jobId);
+            dao.updateJob(job);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ HrApiDay08/src/main/java/org/example/dto/JobFilterDto.java
@@ -0,0 +1,33 @@
+package org.example.dto;
+
+import jakarta.ws.rs.QueryParam;
+
+public class JobFilterDto {
+    private @QueryParam("min_salary") Double min_salary;
+    private @QueryParam("limit") Integer limit;
+    private @QueryParam("offset") int offset;
+
+    public Double getMin_salary() {
+        return min_salary;
+    }
+
+    public void setMin_salary(Double min_salary) {
+        this.min_salary = min_salary;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+}
